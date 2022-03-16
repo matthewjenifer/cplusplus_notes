01fc948136f600c5f42c653de2cfd66ba6940ebd
MATTHEW JENIFER <matthew36.j@gmail.com>
2022-03-16 18:32:16 -0400

    first commit

---
+++ main_notes.cpp
@@ -0,0 +1,14 @@
+// https://www.onlinegdb.com/online_c++_compiler | https://www.sololearn.com/learning/1051
+
+#include <iostream>
+
+using namespace std;
+
+int
+main ()				// cout is used to perform output on the standard output device which is usually the display screen. cout is used in combination with the insertion operator <<. Note that you can add multiple insertion operators after cout.
+{
+  cout << "Hello World";
+  cout << " This " << "is " << "awesome!";	// In C++, the semicolon is used to terminate a statement. Each statement must end with a semicolon. It indicates the end of one logical expression.
+
+  return 0;
+}
