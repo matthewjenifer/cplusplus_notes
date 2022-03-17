c41d77c3812c293bd238836028ca1de06c997043
MATTHEW JENIFER <matthew36.j@gmail.com>
2022-03-16 21:18:39 -0400

    integers and sum..

--- main_notes.cpp
+++ main_notes.cpp
@@ -12,10 +12,33 @@ int
 main ()				// cout is used to perform output on the standard output device which is usually the display screen. cout is used in combination with the insertion operator <<. Note that you can add multiple insertion operators after cout.
 {
   int myFirstIntVariable = 1; // The C++ programming language is case-sensitive, so myVariable and myvariable are two different identifiers.
+  //int a, b; // defines two variables of type int
+  int a = 5;
+  int b = 3;
+
+  int c; // You have the option to assign a value to the variable at the time you declare the variable or to declare it and assign a value later. You can also change the value of a variable.
+  int d = 42;
+  c = 10;
+  d = 9; 
+
+  int num, num2; // To enable the user to input a value, use cin in combination with the extraction operator (>>). The variable containing the extracted data follows the operator. The following example shows how to accept user input and store it in the num variable:
+  cout << "Please enter a number \n";
+  cin >> num; // As with cout, extractions on cin can be claimed to request more than one input in a single statement: cin >> a >> b; When the program runs, it displays the message "Please enter a number", and then waits for the user to enter a number and press Enter, or Return. The entered number is stored in the variable num. The program will wait for as long as the user needs to type in a number.
+  cout << "Gimmie one mo \n"; // You can accept user input multiple times throughout the program:
+  cin >> num2;
+
+  int sum = a + b; 
+  int sum2 = c + d;
+  int sum3 = num + num2;
   cout << "Watup Doe" << endl; // The cout object does not insert a line break at the end of the output. One way to print two lines is to use the endl manipulator, which will put in a line break. 
   cout << "Oh " << "we on " << "C++ now! \n"; // The new line character \n can be used as an alternative to endl. Using a single cout statement with as many instances of \n as your program requires will print out multiple lines of text. The backslash (\) is called an escape character, and indicates a "special" character.	
   cout << "..but will \n this work?\n"; // In C++, the semicolon is used to terminate a statement. Each statement must end with a semicolon. It indicates the end of one logical expression.
-  cout << myFirstIntVariable;
+  cout << myFirstIntVariable<<endl;
+  cout << sum << endl; // prints: 8
+  cout << sum2 << endl; // prints: 19
+  cout << d << endl; // prints: 9
+  cout << "Your sum is: " << sum3 << endl; 
+
   return 0;
 }
 
